77139d16f43cc3fc3a54794b6934cde5884ad9a4
josue-cloud <64337848+josue-cloud@users.noreply.github.com>
2023-08-02 13:58:52 -0700

    Add files via upload

--- sound.js
+++ sound.js
@@ -1,3 +1,34 @@
-var audio = new Audio('y2mate.com - Minecraft.mp3');
-audio.loop = true;
-audio.play();
+//this function will add a sound element to our html page
+//using the DOM (document object model)
+function addSound(){
+    //create an html audio element
+    var soundElem = document.createElement("AUDIO");
+
+    //sets the id so you can refer to the element easily
+    //-in future javascript code.
+    soundElem.setAttribute("id","mySound");
+    //set the file name of the sound file
+    soundElem.setAttribute("src","space.m4a");
+    //shows the controls for the audio
+    soundElem.setAttribute("controls","controls");
+
+    //after the audio element is set up, add it to the webpage
+    document.body.appendChild(soundElem);
+
+//now unhide the play and pause button
+    document.getElementById("btnPlay").hidden = false;
+    document.getElementById("mySound").hidden = false;
+}
+
+//this function will be fired from the btnPlay click event
+//it will play the audio
+function play(){
+    var audio = document.getElementById("mySound");
+    audio.play();
+}
+
+//this function will be fired from the btnPause click event
+//it will pause the audio
+function pause(){
+    
+}
